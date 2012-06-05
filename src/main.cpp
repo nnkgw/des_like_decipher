2cfef88965e915f72701c4270cef0d47205e2766
nnkgw <nkgw@s241.xrea.com>
2012-06-05 18:20:10 +0900

    add S table

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <vector>
 
 char K[] = { 1,0,1,0,1,0,1,0,1,0,1,0 };
 char E[] = { 1,2,3,4,5,6,7,8,1,2,3,4 };  // 1 origin
@@ -16,10 +17,14 @@ char S[2][64] = {                        // 0 origin
       13,  8, 10,  1,  3, 15,  4,  2, 11,  6,  7, 12,  0,  5, 14,  9, },
 };
 
-void s_transform(int n, char* preS, char* postS){
+int s_box(int n, char* preS) {
   int row    = preS[0]*2 + preS[5];
   int column = preS[1]*8 + preS[2]*4 + preS[3]*2 + preS[4];
-  int value  = S[n][row*16+column];
+  return S[n][row*16+column];
+}
+
+void s_transform(int n, char* preS, char* postS){
+  int value  = s_box(n, preS);
   int bit = 8;
   for (int i = 0; i < 4; i++) {
     postS[i] = (value & bit) ? 1 : 0;
@@ -27,9 +32,9 @@ void s_transform(int n, char* preS, char* postS){
   }
 }
 
-void bin_to_string(char* dst, char* src) {
-  char num_str[8];
-  for (int i = 0; i < 8; i++) {
+void bin_to_string(char* dst, char* src, int num) {
+  char num_str[num];
+  for (int i = 0; i < num; i++) {
     dst[i] = src[i] + '0';
   }
 }
@@ -40,10 +45,10 @@ int feistel_function(char* out_R, char* in_R) {
   for (int i = 0; i < 12; i++) { preS[i] = in_R[ E[i]-1 ]; }  // extension E
   for (int i = 0; i < 12; i++) { preS[i] ^= K[i];          }  // xor K
   char num_str[8];
-  bin_to_string(num_str, preS);
+  bin_to_string(num_str, preS, 8);
   if (strncmp(&num_str[0],"010011",6) == 0) {
     s_transform(0, &preS[0], &postS[0]);
-    bin_to_string(num_str, postS);
+    bin_to_string(num_str, postS, 8);
     if (strncmp(&num_str[0],"0110",4) == 0) {
       return 0;
     }
@@ -54,10 +59,22 @@ int feistel_function(char* out_R, char* in_R) {
 void dec_to_bin(char* dst, int dec, int num) {
   int bit = 1;
   for (int i = 0; i < num; i++) {
-    if (dec & bit) { dst[i] = 1; }
-    else           { dst[i] = 0; }
+    if (dec & bit) { dst[num-i-1] = 1; }
+    else           { dst[num-i-1] = 0; }
     bit <<= 1;
   }
+}
+
+void bin_to_dec(int* dec, char* bin, int num) {
+  int n = 0;
+  for (int i = 0; i < num; i++) {
+    switch(bin[num-i-1]) {
+    case 1: n += 1;  // fall through
+    case 0: n *= 2; break;
+    }
+  }
+  n /= 2;
+  *dec = n;
 }
 
 void print_result(char* array, int num) {
@@ -81,5 +98,41 @@ int main(int argc, char* argv[]) {
     // 111001 xor 
     // 010011  
     // 101010 = K
+  }
+  {
+    printf("\n");
+    std::vector<char> s[16];
+    for(int i =0; i < 64; i++){
+      char pre[6];
+      char post[4];
+      dec_to_bin(pre, i, 6);
+      int dec = s_box(0, pre);
+      s[dec].push_back(i);
+    }
+    for(int i = 0; i < 16; i++) {
+      size_t count = s[i].size();
+      printf("%02d | ", i);
+      for(int j = 0; j < count; j++) {
+        printf( "%02d ", s[i][j] );
+      }
+      printf("\n");
+    }
+    printf("\n");
+    for(int i = 0; i < 16; i++) {
+      size_t count = s[i].size();
+      char dec[6];
+      char bin[6+1];
+      dec_to_bin(dec, i, 4);
+      bin_to_string(bin, dec, 4);
+      bin[4] = 0;
+      printf("%2s | ", bin);
+          for(int j = 0; j < count; j++) {
+              dec_to_bin(dec, s[i][j], 6);
+              bin_to_string(bin, dec, 6);
+              bin[6] = 0;
+              printf( "%2s ",  bin);
+          }
+          printf("\n");
+      }
   }
 }
